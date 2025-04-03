5aedad02379649525323330fc6dd981843d2f2a4
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2025-04-03 13:18:12 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,20 @@
+def primeiroNaoRepete(texto):
+    contagem = {}
+    tam = len(texto)
+    if tam == 0:
+        return "vazio"
+    for caractere in texto:
+        if caractere in contagem:
+            contagem[caractere] += 1
+        else:
+            contagem[caractere] = 1
+    for i in range(tam):
+        if contagem[texto[i]] == 1:
+            return i
+    return -1
+
+
+# Testes
+print(primeiroNaoRepete("abacabad"))
+print(primeiroNaoRepete("aabbcc"))
+print(primeiroNaoRepete(""))
