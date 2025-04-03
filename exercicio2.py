3f1ad60ea66bc73775f5660df50a4607013f430f
ldavila43 <160516394+ldavila43@users.noreply.github.com>
2025-04-03 13:36:10 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,18 @@
+def primeiro_item(x):
+    return x[0] # função para pegar o primeiro elemento do intervalo
+
+
+def mesclaIntervalos(intervalos):
+    intervalos.sort(key=primeiro_item) #utilizando o primeiro elemento do intervalo como key
+    for i in range(len(intervalos) - 1):
+        if intervalos[i][1] >= intervalos[i + 1][0]:
+            intervalos[i] = (intervalos[i][0], max(intervalos[i][1], intervalos[i + 1][1]))
+            del intervalos[i + 1]
+            return mesclaIntervalos(intervalos)
+    return intervalos
+
+
+# Testes
+print(mesclaIntervalos([(1, 3), (2, 6), (8, 10), (15, 18)]))
+print(mesclaIntervalos([(1, 4), (4, 5)]))
+print(mesclaIntervalos([(1, 4), (2, 3), (5, 7), (6, 8)]))
